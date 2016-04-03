8b4f19af570771a0bdc0e21bd4de140772b3905e
Johannes Stein <johannesstein@freeze-dev.com>
2016-04-03 02:37:15 +0100

    Use rollup-babel-lib-bundler

--- rollup.cjs.config.js
+++
@@ -1,6 +0,0 @@
-import config from './rollup.config';
-
-config.format = 'cjs';
-config.dest = 'lib/index.js';
-
-export default config;
--- rollup.umd.config.js
+++
@@ -1,7 +0,0 @@
-import config from './rollup.config';
-
-config.format = 'umd';
-config.dest = 'umd/stringreader.js';
-config.moduleName = 'StringReader';
-
-export default config;
